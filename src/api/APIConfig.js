c8287b99f3a20b4219f285be4c96bcdc1ea5d22c
AlexJester147 <vardy2016@lenta.ru>
2023-12-31 14:36:11 +0300

    add api config and requests

---
+++ src/api/APIConfig.js
@@ -0,0 +1,6 @@
+export const apiCatalog = "https://api.digiseller.ru/api/categories?";
+export const apiProducts = "https://api.digiseller.ru/api/shop/products?";
+export const apiProductInfo = "https://api.digiseller.ru/api/products/";
+export const apiOptions = "https://api.digiseller.ru/api/purchases/options";
+export const addToCartApi = "https://shop.digiseller.ru/xml/shop_cart_add.asp";
+export const storeKey = 857910;
---
+++ src/api/requests.js
@@ -0,0 +1,40 @@
+import { storeKey, apiCatalog, apiProducts, apiProductInfo } from "./APIConfig";
+
+export async function getCategoryDataById(id) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(
+    `${apiCatalog}seller_id=${storeKey}&category_id=${id}`,
+    options
+  );
+  return response.json();
+}
+
+export async function getProductsByCategoryId(id) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(
+    `${apiProducts}seller_id=${storeKey}&category_id=${id}&rows=30`,
+    options
+  );
+  return response.json();
+}
+
+export async function getProductInfoById(id) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(`${apiProductInfo}/${id}/data`, options);
+  return response.json();
+}
