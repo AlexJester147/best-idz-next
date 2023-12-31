0b85af2da25b0450123ce44adaff33d897de4621
AlexJester147 <vardy2016@lenta.ru>
2023-12-31 12:21:49 +0300

    install sass, add global styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "../styles/globals.scss";
+import "../scss/app.scss";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
