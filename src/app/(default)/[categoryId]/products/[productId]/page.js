77849ec7355adcd46244eef9ffa01337cac52135
AlexJester147 <vardy2016@lenta.ru>
2024-01-08 17:26:35 +0300

    add product images to product page

--- src/app/(default)/[categoryId]/products/[productId]/page.js
+++ src/app/(default)/[categoryId]/products/[productId]/page.js
@@ -1,8 +1,10 @@
 import { getProductInfoById } from "@/api/requests";
 import Breadcrumbs from "@/components/Breadcrumbs";
+import Image from "next/image";
 
 export default async function Product({ params }) {
   const data = await getProductInfoById(params.productId);
+  const imagePath = `/images/${params.categoryId}/${params.productId}.png`;
 
   return (
     <>
@@ -13,6 +15,12 @@ export default async function Product({ params }) {
       />
       {
         <div className="product">
+          <Image
+            src={imagePath}
+            width={300}
+            height={300}
+            alt={data.product.name}
+          />
           <div>{data.product.name}</div>
           <div>{data.product.info}</div>
           <div>{JSON.stringify(data.product)}</div>
